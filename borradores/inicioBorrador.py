ba03211edd225d5b3563ebf84b34417dc44a7538
Orlando <24heyerunsu@gmail.com>
2024-06-02 21:42:54 -0500

    Avance significativo en la pestaña principal

--- borradores/inicioBorrador.py
+++ borradores/inicioBorrador.py
@@ -33,8 +33,6 @@ def main(page: ft.page):
         agregar_Equipo.open = False
         agregar_Equipo.update()
 
-
-
     def close_dlg(e):
         dlg_modal.open = False
         page.update()
@@ -43,6 +41,20 @@ def main(page: ft.page):
         page.dialog = dlg_modal
         dlg_modal.open = True
         page.update()
+
+    def change_date(e):
+        dateString = (date_picker.value.strftime("%d/%m/%Y"))
+        date_button.text = dateString
+        date_button.update()
+        print(dateString)
+
+    def date_picker_dismissed(e):
+        dateString = (date_picker.value.strftime("%d/%m/%Y"))
+        date_button.text=dateString
+        date_button.update()
+        print(dateString)
+
+
 
 
     page.navigation_bar = ft.NavigationBar(
@@ -74,6 +86,7 @@ def main(page: ft.page):
     marcaEquipo = "EPSON"
     celularCliente = "0987777777"
 
+
     nombreEquipoLabel = ft.Text(nombreEquipo, color='#3F4450', size=19, weight='w500')
     estadoEquipoLabel = ft.Text("En estado: ", color='#3F4450', size=17, weight='w400',
                                                     spans=[ft.TextSpan(estadoEquipo, ft.TextStyle(color='#FF914D', weight='w500'))])
@@ -82,6 +95,30 @@ def main(page: ft.page):
     nombreClienteEquipoLabel = ft.Text(nombreCliente, color='#3F4450', size=17, weight='w400')
     descriEquipoLabel = ft.Text("Observaciones: ", color='#3F4450', size=15, weight='w500',
                                                     spans=[ft.TextSpan(observaciones, ft.TextStyle(color='#3F4450', weight='w400'))], max_lines=5)
+
+    # Formulario de Editar
+    nombreEquipoEdit = ft.TextField(label="Nombre de equipo", value=nombreEquipo, read_only=True)
+    nuevaObservacion = ft.TextField(label="Actualizar información", multiline=True, max_lines=5, disabled=True)
+    nuevoEstado = ft.TextField(label="Nuevo Estado", disabled=True)
+    equipoMarcaEdit = ft.TextField(label="Marca", value=marcaEquipo, read_only=True)
+    nombreClienteEdit = ft.TextField(label="Nombre Cliente", value=nombreCliente, width=290, read_only=True)
+
+
+    date_picker = ft.DatePicker(
+        on_change=change_date,
+        on_dismiss=date_picker_dismissed,
+        field_label_text="Ingresa una fecha",
+        first_date=datetime.datetime(2024, 6, 1),
+    )
+
+    date_button = ft.ElevatedButton(
+        "Fecha Actualización",
+        icon=ft.icons.CALENDAR_MONTH,
+        on_click=lambda _: date_picker.pick_date(),
+        width=250,
+        color='#3EC99D',
+        disabled=True,
+    )
 
 
     dlg_modal = ft.AlertDialog(
@@ -116,19 +153,49 @@ def main(page: ft.page):
         ft.Container(
             ft.Column(
                 [
+                    # Contenido del formulario antes de los botones
                     ft.Container(ft.Text("Información de ", size=35, spans=[ft.TextSpan("Equipo", ft.TextStyle(color='#3EC99D'))]), alignment=ft.alignment.center),
+
+                    # ID ---------------------------------------------------------------------------------------
                     ft.Container(ft.Text("ID: ", size=22,spans=[ft.TextSpan(id_equipo, ft.TextStyle(color='#3EC99D'))]),alignment=ft.alignment.center),
+
+                    # Boton Ver Historial ----------------------------------------------------------------------
                     ft.Container(ft.TextButton("Ver historial", style=ft.ButtonStyle(color=ft.colors.WHITE)), alignment=ft.alignment.center_right),
-                    ft.Container(ft.TextField(label="Nombre de equipo", value=nombreEquipo), bgcolor=ft.colors.WHITE10),
+
+                    # TextField nombre del equipo --------------------------------------------------------------
+                    ft.Container(nombreEquipoEdit, bgcolor=ft.colors.WHITE10),
+
+                    # Text Fields con la marca, y el nombre del cliente ----------------------------------------
                     ft.Container(ft.Row([
-                        ft.Container(ft.TextField(label="Marca", value=marcaEquipo),bgcolor=ft.colors.WHITE10),
-                        ft.Container(ft.TextField(label="Nombre Cliente", value=nombreCliente, width=290), bgcolor=ft.colors.WHITE10),
+                        ft.Container(equipoMarcaEdit,bgcolor=ft.colors.WHITE10),
+                        ft.Container(nombreClienteEdit, bgcolor=ft.colors.WHITE10),
                     ])),
+
+                    # Boton para seleccionar la fecha. Y textfield del Número de contacto del cliente -----------
                     ft.Container(ft.Row([
-                        ft.Container(
-                            ft.TextField(label="Celular Contacto", value=celularCliente, width=300, read_only=True),
-                            bgcolor=ft.colors.WHITE10),
-                    ]))
+                        ft.Container(date_button, padding=ft.padding.only(25)),
+                        ft.Container(ft.TextField(label="Celular Contacto", value=celularCliente, read_only=True),bgcolor=ft.colors.WHITE10, width=290),
+                    ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN),),
+
+                    # Label que muestra el estado actual, y text field para modificar el estado -----------------
+                    ft.Container(ft.Row([
+                        ft.Container(ft.Text("Estado Actual: ", size=18,spans=[ft.TextSpan(estadoEquipo, ft.TextStyle(color='#3EC99D'))]),padding=ft.padding.only(50,13,50,13), bgcolor=ft.colors.WHITE10, border_radius=5,border=ft.border.all(1, color=ft.colors.BLACK)),
+                        ft.Container(nuevoEstado,bgcolor=ft.colors.WHITE10, width=290),
+                    ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN), ),
+
+                    # Texto que muestra la ultima observacion --------------------------------------------------
+                    ft.Container(ft.Text("Observaciones: ", color='#3EC99D', size=16,
+                                        spans=[ft.TextSpan(observaciones, ft.TextStyle(color=ft.colors.WHITE))], max_lines=4), bgcolor=ft.colors.WHITE10, border_radius=5,
+                                 border=ft.border.all(1, color=ft.colors.BLACK), padding=ft.padding.all(12)),
+
+                    # Text Field para la nueva observacion -----------------------------------------------------
+                    ft.Container(nuevaObservacion, bgcolor=ft.colors.WHITE10),
+
+                    # ----------------------------Botones------------------------------------------------------
+                    ft.Container(ft.Row([
+                        ft.ElevatedButton(content=ft.Text('Actualizar Datos', color='white', weight='w300'),bgcolor='#3F4450', on_hover=on_hover, on_click=show_agEq),
+                        ft.ElevatedButton(content=ft.Text('Cerrar Ventana', color='white', weight='w300'),bgcolor='#3F4450', on_hover=on_hover, on_click=close_bs),
+                    ], alignment=ft.MainAxisAlignment.SPACE_AROUND)),
                 ],
                 tight=True, spacing=8
             ),
@@ -404,7 +471,8 @@ def main(page: ft.page):
                 homeTab,
                 clienteTab,
                 historialTab,
-                perfilTab
+                perfilTab,
+                date_button
             ]),
         )
 
@@ -419,6 +487,7 @@ def main(page: ft.page):
     page.window_maximizable = False
     page.overlay.append(editarVer_Equipo)
     page.overlay.append(agregar_Equipo)
+    page.overlay.append(date_picker)
     page.add(inicio)
 
 
